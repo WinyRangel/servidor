d806c78ddb9426da4bad5a0f47bcb9cd49343579
WinyRangel <99991728+WinyRangel@users.noreply.github.com>
2024-04-26 00:25:15 -0600

    Update solicitarRecurso.js

--- controllers/solicitarRecurso.js
+++ controllers/solicitarRecurso.js
@@ -2,7 +2,7 @@ const Recurso = require("../models/recurso.js");
 const { transporter } = require('../nodemailer.js'); // Ajusta la ruta según tu estructura de carpetas
 const Empleado = require("../models/Empleado.js"); // Ajusta la ruta según tu estructura de carpetas
 const Solicitud = require("../models/Solicitud.js"); // Ajusta la ruta según tu estructura de carpetas
-const recurso = require("../models/recurso.js");
+const recurso = require("../models/Recurso.js");
 exports.solicitarRecurso = async (req, res) => {
   try {
       const { idEmpleado, nombre, recurso, marca, comentario, estado } = req.body;
